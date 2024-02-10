5560874c43287c37b47c0d9e9cc7a7f68325d709
JACKS9969 <101617802+JACKS9969@users.noreply.github.com>
2024-02-10 20:36:35 +0530

    new

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
 import './App.css'
-import ImageSlider from './ImageSlider'
-import NumberCounter from './NumberCounter'
+
 
 function App() {
   return (
     <>
-      <ImageSlider/>
-      <NumberCounter/>
+      
     </>
   )
 }
